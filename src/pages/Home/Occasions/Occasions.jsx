93e0c7c93b1966bac31ae445721d6ba881dadcd5
Nishant <pnishant0824@gmail.com>
2026-06-09 23:58:48 +0530

    Improve occasions section UI

--- src/pages/Home/Occasions/Occasions.jsx
+++ src/pages/Home/Occasions/Occasions.jsx
@@ -2,7 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Occasions.css";
 
-const occasions = ["Birthday", "Anniversary", "Wedding", "Festival", "Custom"];
+const occasions = [
+  {
+    name: "Birthday",
+    image:
+      "https://images.unsplash.com/photo-1464349153735-7db50ed83c84?w=800",
+  },
+  {
+    name: "Anniversary",
+    image:
+      "https://images.unsplash.com/photo-1516589178581-6cd7833ae3b2?w=800",
+  },
+  {
+    name: "Wedding",
+    image:
+      "https://images.unsplash.com/photo-1519741497674-611481863552?w=800",
+  },
+  {
+    name: "Festival",
+    image:
+      "https://images.unsplash.com/photo-1513151233558-d860c5398176?w=800",
+  },
+  {
+    name: "Custom",
+    image:
+      "https://images.unsplash.com/photo-1516321318423-f06f85e504b3?w=800",
+  },
+];
 
 export default function Occasions() {
   return (
@@ -15,11 +41,18 @@ export default function Occasions() {
       <div className="occasion-slider occasion-grid">
         {occasions.map((occasion) => (
           <Link
-            key={occasion}
-            to={`/shop?occasion=${occasion}`}
+            key={occasion.name}
+            to={`/shop?occasion=${occasion.name}`}
             className="occasion-card"
           >
-            <span>{occasion}</span>
+            <img src={occasion.image} alt={occasion.name} />
+
+            <div className="occasion-card-content">
+              <h3>{occasion.name}</h3>
+              <p>Explore cards</p>
+            </div>
+
+            <span className="occasion-arrow">→</span>
           </Link>
         ))}
       </div>
